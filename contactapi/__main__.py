95f866d2f5a66b5e11b34177f6c0c8075332ada7
Yulai202020 <ngmyulai@gmail.com>
2023-07-19 16:05:46 +0500

    pypi

--- contactapi/__main__.py
+++ contactapi/__main__.py
@@ -1,9 +1,8 @@
 import uvicorn
 from main import app
-import os
 
 def run():
-    uvicorn.run(app, host = "0.0.0.0", port = os.environ.get("PORT"))
+    uvicorn.run(app, host = "0.0.0.0", port = 8000)
 
 if __name__ == "__main__" :
     run(host = "0.0.0.0", port = 8000)
