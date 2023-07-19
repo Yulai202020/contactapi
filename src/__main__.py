4d6736d25a4a00bf03b747b19148b9641c33a6ca
Yulai202020 <ngmyulai@gmail.com>
2023-07-19 15:08:40 +0500

    pypi

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,8 @@
-from main import run
+import uvicorn
+from main import app
+
+def run(host: str, port: int):
+    uvicorn.run(app, host = host, port = port)
 
 if __name__ == "__main__" :
     run(host = "0.0.0.0", port = 8000)
